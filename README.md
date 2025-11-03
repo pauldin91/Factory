3abe9b2ae29740bbeb0b39ceaa5be064013a4141
pauldin91 <pauldin91@gmail.com>
2025-11-03 21:09:46 +0200

    add example

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # Factory Pattern Implementation
 
 ## Features
+
 - **Generic Factory**
-   - Create and cache instances that implement an interface
+  - Create and cache instances that implement an interface
 - **Thread-Safe**
 - **Dependency Injection Support**
-- **Lots of examples as Tests** 
+- **Lots of examples as Tests**
 - **Flexible Resolution**
   Creates objects based on
   - Generic type argument
@@ -13,35 +14,41 @@
   - By type name (or simply the name of the class)
     and a type in implementations assembly
 
-
 ## Example
 
 ### The Interface
 
 #### IService.cs
+
 ```csharp
 public interface INotificationService
 {
     void Notify();
 }
 ```
-### The Implementations 
+
+### The Implementations
 
 #### EmailService.cs
+
 ```csharp
 public class EmailService : INotificationService
 {
     public void Notify() => Console.WriteLine("Sending Email...");
 }
 ```
+
 #### SmsService.cs
-```csharp 
+
+```csharp
 public class SmsService : INotificationService
 {
     public void Notify() => Console.WriteLine("Sending SMS...");
 }
 ```
+
 #### Usage
+
 ```csharp
 public class DemoService(IFactory<INotificationService> factory)
 {
@@ -52,7 +59,7 @@ public class DemoService(IFactory<INotificationService> factory)
     email.Notify();
 
     var sms = factory.GetOrAddInstance(typeof(SmsService));
-    sms.Notify(); 
+    sms.Notify();
 
     // Here EmailService is used to get the Assembly with the implemenation types
     var smsByName = factory.GetOrAddInstance<EmailService>("SmsService");
@@ -60,3 +67,43 @@ public class DemoService(IFactory<INotificationService> factory)
   }
 }
 ```
+
+#### A more realistic scenario
+
+#### SmsService.cs
+
+````csharp
+{
+  "RealWorldOptions":{
+
+    "CodeToServiceMap":{
+      "1":"EmailNotificationService",
+      "2":"PushNotificationService",
+      "3":"SmsNotificationService",
+    }
+  }
+}
+
+public class RealWorldOptions {
+  public Dictionary<string,string> CodeToServiceMap { get; set; }
+}
+
+```csharp
+public class RealWorldService
+{
+  private readonly Dictionary<string,string> _codeToServiceCache = new();
+  private readonly IFactory<INotificationService> _factory;
+  public RealWorldService(IConfiguration configuration,IFactory<INotificationService> factory){
+    _factory = factory;
+    _codeToServiceCache = ConfigurationBinder.Get<RealWorldOptions>(configuration.GetRequiredSection(nameof(RealWorldOptions)))!.CodeToServiceMap;
+  }
+
+  public void DemoByCode(string code)
+  {
+    if(_codeToServiceCache.TryGetValue(code,out var serviceName)){
+      var notificationService = _factory.GetOrAddInstance<EmailNotificationService>(serviceName);
+      notificationService.Notify();
+    }
+  }
+}
+````
--- tests/Factory.Tests/FactoryTests.cs
+++ tests/Factory.Tests/FactoryTests.cs
@@ -11,16 +11,16 @@ namespace Factory.Tests
     {
 
         private readonly IFactory<IEncoder> _factory = new FactoryWrapperImpl<IEncoder>();
-        private readonly Dictionary<string,string> cfgSection = new()
+        private readonly Dictionary<string, string> cfgSection = new()
         {
-                
+
             { "EncoderOptions:Encoders:.abc", "EncoderFormatA"},
             { "EncoderOptions:Encoders:.bcd", "EncoderFormatB"},
             { "EncoderOptions:Encoders:.cde", "EncoderFormatC"},
             { "EncoderOptions:Encoders:.def", "EncoderFormatD"},
             { "EncoderOptions:Encoders:.efg", "EncoderFormatE"},
         };
-        private readonly Dictionary<string,Type> _actualTypes = new()
+        private readonly Dictionary<string, Type> _actualTypes = new()
         {
             { ".abc", typeof(EncoderFormatA)},
             { ".bcd", typeof(EncoderFormatB)},
@@ -28,8 +28,8 @@ namespace Factory.Tests
             { ".def", typeof(EncoderFormatD)},
             { ".efg", typeof(EncoderFormatE)},
         };
-        
-        
+
+
         [Test]
         public void TestGetOrAddInstance_ShouldReturnInstances_ByGenericArgument()
         {
@@ -38,45 +38,45 @@ namespace Factory.Tests
             var typeE = typeof(EncoderFormatE);
 
             Assert.That(typeA.IsEquivalentTo(_factory.GetOrAddInstance<EncoderFormatA>().GetType()));
-            
+
             Assert.That(typeC.IsEquivalentTo(_factory.GetOrAddInstance<EncoderFormatC>().GetType()));
             Assert.That(typeE.IsEquivalentTo(_factory.GetOrAddInstance<EncoderFormatE>().GetType()));
         }
-        
+
         [Test]
         public void TestGetOrAddInstance_ShouldReturnInstances_ByType()
         {
-            
+
             var types = typeof(EncoderFormatA)
                 .Assembly
                 .GetTypes()
                 .Where(t => t.IsAssignableTo(typeof(IEncoder)) && !t.IsAbstract && !t.IsInterface)
-                .ToDictionary(s=>s.Name, s=>s);
-            
+                .ToDictionary(s => s.Name, s => s);
+
             foreach (var item in types)
             {
                 var instance = _factory.GetOrAddInstance(item.Value);
                 Assert.That(instance.GetType(), Is.EqualTo(item.Value));
                 Assert.That(instance.GetMsg(), Is.EqualTo($"Inside {item.Key}"));
-                
+
             }
         }
-        
+
 
         [Test]
         public void TestGetOrAddInstance_ShouldThrowInvalidCastException_ForNonImplementingClasses()
         {
             var typeNot = typeof(NotEncoder);
 
-           Assert.Throws<InvalidCastException>(() => _factory.GetOrAddInstance(typeNot).GetType());
+            Assert.Throws<InvalidCastException>(() => _factory.GetOrAddInstance(typeNot).GetType());
         }
 
 
-        
+
         [Test]
         public void TestGetOrAddInstance_ShouldReturnInstances_ByTypeInAssembly()
         {
-            
+
             foreach (var item in cfgSection)
             {
                 var instance = _factory.GetOrAddInstance<EncoderFormatA>(item.Value);
@@ -84,7 +84,7 @@ namespace Factory.Tests
                 Assert.That(instance.GetMsg(), Is.EqualTo($"Inside {item.Value}"));
             }
         }
-        
+
         [Test]
         public void TestGetOrAddInstance_ShouldReturnInstances_ByTypeNameInAssemblyBasedOnConfiguration()
         {
@@ -93,10 +93,10 @@ namespace Factory.Tests
                 .Build();
 
             var t = cfg.ToString();
-            
+
             var options = new EncoderOptions();
-            cfg.Bind(nameof(EncoderOptions).Split(".").Last(),options);
-            
+            cfg.Bind(nameof(EncoderOptions).Split(".").Last(), options);
+
             foreach (var item in options.Encoders)
             {
                 var instance = _factory.GetOrAddInstance<EncoderFormatA>(item.Value);
